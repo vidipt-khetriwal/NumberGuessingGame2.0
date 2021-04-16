f09f6992fb8209e99e9f9000b759429e193552b2
Vidipt Khetriwal <mohangoyal@Vidipts-MacBook-Pro.local>
2021-04-16 19:10:27 +0530

    Modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let count = 4;
+let count = 3;
 
 function check(c_id) 
 {
@@ -9,6 +9,7 @@ function check(c_id)
         document.getElementById(x).style.background = "green";
         document.getElementById(x).style.border = "none";
         document.getElementById("res").textContent = "Bravo! You guessed it right. This page will reload in 5 sec.";
+        on();
         setTimeout(function() {
             location.reload();
           }, 5000);
@@ -18,17 +19,21 @@ function check(c_id)
         document.getElementById(x).style.background = "red";
         document.getElementById(x).style.border = "none";
         document.getElementById("res").textContent =
-        "Sorry, wrong answer. Please try again! "+(count-1)+" chances left!";
+        "Sorry, wrong answer. Please try again! "+(count)+" chances left!";
 
     }
     
 
-    if (count === 1) 
+    if (count === 0) 
     {
         document.getElementById("res").textContent =
         "Your 3 chances are over. The page will be reloaded in 5 sec.";
         setTimeout(function() {
             location.reload();
           }, 5000);
+        on();
     }
 }
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
